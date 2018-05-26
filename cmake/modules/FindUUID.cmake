b9a009343cfc895cc9691abaf81515050a76bd37
softboy <zzu_softboy@163.com>
2018-05-26 22:28:20 +0800

    add findICU cmake script

--- cmake/modules/FindUUID.cmake
+++ cmake/modules/FindUUID.cmake
@@ -1,47 +1,25 @@
-# Find libicu's libraries
-
-include(FindPackageHandleStandardArgs)
+# Find libuuid
 
 find_package(PkgConfig)
+pkg_check_modules(PC_UUID QUIET uuid)
+set(UUID_DEFINITIONS ${PC_UUID_CFLAGS_OTHER})
 
-set(ICU_REQUIRED)
-foreach(MODULE ${ICU_FIND_COMPONENTS})
-   string(TOUPPER "${MODULE}" MODULE)
-   string(TOLOWER "${MODULE}" module)
-   list(APPEND ICU_REQUIRED
-      ICU_${MODULE}_INCLUDE_DIR ICU_${MODULE}_LIBRARIES)
-   
-   pkg_check_modules(PC_ICU_${MODULE} QUIET icu-${module})
-   if(NOT ${PKGCONFIG_FOUND})
-      # PkgConfig doesn't exist on this system, so we manually provide hints via CMake.
-      set(PC_ICU_${MODULE}_INCLUDE_DIRS "${ICU_${MODULE}_INCLUDE_DIRS}")
-      set(PC_ICU_${MODULE}_LIBRARY_DIRS "${ICU_${MODULE}_LIBRARY_DIRS}")
-   endif()
-   
-   if((${PC_ICU_${MODULE}_FOUND}) OR (NOT ${PKGCONFIG_FOUND}))
-      set(ICU_${MODULE}_DEFINITIONS ${PC_ICU_${MODULE}_CFLAGS_OTHER})
-      
-      find_path(ICU_${MODULE}_INCLUDE_DIR unicode
-         HINTS ${PC_ICU_${MODULE}_INCLUDEDIR} ${PC_ICU_${MODULE}_INCLUDE_DIRS})
-      set(ICU_${MODULE}_INCLUDE_DIRS ${ICU_${MODULE}_INCLUDE_DIR})
-      
-      find_library(ICU_${MODULE}_LIBRARY NAMES icu${module} ${ICU_${MODULE}_LIB_NAME}
-         HINTS ${PC_ICU_${MODULE}_LIBDIR} ${PC_ICU_${MODULE}_LIBRARY_DIRS})
-      set(ICU_${MODULE}_LIBRARIES ${ICU_${MODULE}_LIBRARY})
-   endif()
-endforeach()
+find_path(UUID_INCLUDE_DIR uuid/uuid.h
+    HINTS ${PC_UUID_INCLUDEDIR} ${PC_UUID_INCLUDE_DIRS})
+set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
 
-foreach(sdk ANDROID;FREEBSD;LINUX;WINDOWS;HAIKU)
-   foreach(MODULE ${ICU_FIND_COMPONENTS})
-      string(TOUPPER "${MODULE}" MODULE)
-      if("${SWIFT_${sdk}_ICU_${MODULE}_INCLUDE}" STREQUAL "")
-         set(SWIFT_${sdk}_ICU_${MODULE}_INCLUDE ${ICU_${MODULE}_INCLUDE_DIRS})
-      endif()
-      if("${SWIFT_${sdk}_ICU_${MODULE}}" STREQUAL "")
-         set(SWIFT_${sdk}_ICU_${MODULE} ${ICU_${MODULE}_LIBRARY})
-      endif()
-   endforeach()
-endforeach()
+# On OS X we don't need the library
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  set(UUID_REQUIRED UUID_INCLUDE_DIR)
+else()
+  find_library(UUID_LIBRARY NAMES uuid
+      HINTS ${PC_UUID_LIBDIR} ${PC_UUID_LIBRARY_DIRS})
+  set(UUID_LIBRARIES ${UUID_LIBRARY})
 
-find_package_handle_standard_args(ICU DEFAULT_MSG ${ICU_REQUIRED})
-mark_as_advanced(${ICU_REQUIRED})
+  set(UUID_REQUIRED UUID_INCLUDE_DIR UUID_LIBRARY)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(UUID DEFAULT_MSG ${UUID_REQUIRED})
+
+mark_as_advanced(${UUID_REQUIRED})
