fbe0c16b721a8e4684cc7d7de8d4703689a3dcec
softboy <zzu_softboy@163.com>
2018-05-27 09:52:23 +0800

    add macro of polarPHP PolarConfig.h.cmake

---
+++ include/polar/global/PolarConfig.h.cmake
@@ -0,0 +1,27 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2017 - 2018 polarPHP software foundation
+// Copyright (c) 2017 - 2018 zzu_softboy <zzu_softboy@163.com>
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://polarphp.org/LICENSE.txt for license information
+// See http://polarphp.org/CONTRIBUTORS.txt for the list of polarPHP project authors
+// 
+// Created by softboy on 2018/05/27.
+
+#ifndef POLAR_GLOABL_CONFIG_H
+#define POLAR_GLOABL_CONFIG_H
+
+/* Major version of the polarphp API */
+#define POLAR_VERSION_MAJOR ${POLAR_VERSION_MAJOR}
+
+/* Minor version of the polarphp API */
+#define POLAR_VERSION_MINOR ${POLAR_VERSION_MINOR}
+
+/* Patch version of the polarphp API */
+#define POLAR_VERSION_PATCH ${POLAR_VERSION_PATCH}
+
+/* polarphp version string */
+#define POLAR_VERSION_STRING "${POLAR_PACKAGE_VERSION}"
+
+#endif
